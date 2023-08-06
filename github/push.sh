1cd6b35049bf813d4a6a050a16b474be60c7c47e
Al Muhdil Karim <al.muhdil.0@gmail.com>
2023-08-06 16:04:53 +0700

    engine-update

--- github/push.sh
+++ github/push.sh
@@ -55,6 +55,7 @@ function github_push_module_admin() {
     if [ -d "admin" ]; then
         cd admin ;
         github_push_remote_update;
+        cd ..
         echo "Falcon admin module is update";
     fi
 }
