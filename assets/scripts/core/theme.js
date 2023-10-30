6400e92502f09569d1384a6596c64b1aae9822c6
almuhdilkarim13 <almuhdil.karim@uinjkt.ac.id>
2023-10-30 19:18:20 +0700

    add tabs feature

--- assets/scripts/core/theme.js
+++ assets/scripts/core/theme.js
@@ -1,23 +1,90 @@
-// darkmode switcher
 export class themebasics {
 
-    
+
     constructor(){
-        let dropdawn = document.querySelector( '.dropdown' );
-        if ( dropdawn ) {
-            this.dropdown();
-        }
-        let darkmode = document.querySelector( '.theme-menu' );
-        if ( darkmode ) {
-            this.darkmode();
+        
+        // tablayout engines
+        if ( document.querySelector( '.tabs' ) ) {
+            this.tabpreps();
+            this.tabmodul();
         }
 
+        // dropdown engines
+        if ( document.querySelector( '.dropdown' ) ) {
+            this.dropdown();
+        }
+   
+        // themes darkmode
+        if ( document.querySelector( '.theme-menu' ) ) {
+            this.darkmode();
+        }
+   
         // progressive web aplication
-        let pwascript = document.querySelector('meta[name="pwa-engines"]');
-        if ( pwascript ) {
+        if ( document.querySelector('meta[name="pwa-engines"]') ) {
             console.log('pwa active')
             this.pwaengine();
         }
+    }
+
+
+    tabpreps() {
+        let builder = Array.prototype.slice.call( document.querySelectorAll( '.tabs.build' ) );
+        let tabcount = 0;
+        // tabs builder
+        builder.forEach( tab => {
+            // tab menu
+            let tabmenus = Array.prototype.slice.call( tab.querySelectorAll( '.tabs-open' ) );
+            tabmenus.forEach( action => {
+                if ( tabcount === 0 ) {
+                    action.classList.add( 'active' );
+                }
+                tabcount ++;
+            });
+            // tab page
+            tabcount = 0;
+            let tabloads = tab.querySelector( '.tabs-load' );
+            let tabdatas = atob(tabloads.dataset.tabs).toLowerCase().replace(/\s/g,'').split( ',' );
+            let tabitems = Array.prototype.slice.call( tabloads.querySelectorAll( 'div' ) );
+            tabitems.forEach( item => {
+                if ( tabcount === 0 ) {
+                    item.classList.remove( 'd-hide' );
+                    item.classList.add( 'active' );
+                }
+                item.setAttribute( 'id', 'tab-'+ tabdatas[tabcount] );
+                tabcount ++;
+            });
+            tab.classList.remove('build');
+        });
+    }
+    
+
+    tabmodul() {
+        let actions = Array.prototype.slice.call( document.querySelectorAll('.tabs-open') );
+        actions.forEach( item => {
+            item.addEventListener('click', (event)=> {
+                let action = event.currentTarget;
+                let parent = action.parentElement.parentElement;
+                let target = parent.querySelector( '#'+action.dataset.target );
+                if ( target ) {
+                    // tabs menu
+                    let getact = Array.prototype.slice.call(parent.querySelectorAll( '.tabs-open' ));
+                    getact.forEach( item => {
+                        item.classList.remove( 'active' );
+                    });
+                    action.classList.add( 'active' );
+                    // tabs page
+                    let getcon = Array.prototype.slice.call(parent.querySelectorAll('.tab-content' ));
+                    getcon.forEach( item => {
+                        if ( item.classList.contains('active') ) {
+                            item.classList.remove( 'active' );
+                            item.classList.add( 'd-hide' );
+                        }
+                    });
+                    target.classList.remove( 'd-hide' );
+                    target.classList.add( 'active' );
+                } 
+            });
+        });
     }
 
 
--- master/content/_index.id.md
+++ master/content/_index.id.md
@@ -15,15 +15,32 @@ draft: false
 ###### karim
 
 
-
-{{< grid columns="2" class="" element="section" >}}
-    {{< column >}}
+<!-- grid example -->
+{{< grid count="2" class="" tag="section" >}}
+    {{< cols id="" class="" >}}
         karim col 1
-    {{< /column >}}
-    {{< column >}}
+    {{< /cols >}}
+    {{< cols id="" class="" >}}
         karim col 2
-    {{< /column >}}
+    {{< /cols >}}
 {{< /grid >}}
+
+
+<!-- tabs example -->
+{{< tabs name="keluarga Al" data="Karim, evi, zaza, rasyid, mama" class="" tag="section" >}}
+    {{< tab >}}
+        karim Tab 1
+    {{< /tab >}}
+    {{< tab >}}
+        karim Tab 2
+    {{< /tab >}}
+    {{< tab >}}
+        karim Tab 3
+    {{< /tab >}}
+    {{< tab >}}
+        karim Tab 4
+    {{< /tab >}}
+{{< /tabs >}}
 
 
 Surabaya - Ketua DPP PDIP Puan Maharani menegaskan Gibran Rakabuming masih menjadi bagian dari partainya. Meski anak Presiden Jokowi tersebut telah menerima pencalonan jadi cawapres dari Golkar.
@@ -31,4 +48,4 @@ Surabaya - Ketua DPP PDIP Puan Maharani menegaskan Gibran Rakabuming masih menja
 
 Ditanya apakah akan menjatuhkan sanksi terhadap Gibran jika akhirnya menerima jadi cawapres Prabowo. Lagi-lagi Puan menegaskan bahwa Gibran belum membuat keputusan apapun.
 
-Baca artikel detikjatim, "Akankah Gibran Dipecat Jika Jadi Cawapres Prabowo, Ini Jawaban Puan" [selengkapnya](https://www.detik.com/jatim/berita/d-6995145/akankah-gibran-dipecat-jika-jadi-cawapres-prabowo-ini-jawaban-puan) .
+Baca artikel detikjatim, "Akankah Gibran Dipecat Jika Jadi Cawapres Prabowo, Ini Jawaban Puan" [selengkapnya](https://www.detik.com/jatim/berita/d-6995145/akankah-gibran-dipecat-jika-jadi-cawapres-prabowo-ini-jawaban-puan).
