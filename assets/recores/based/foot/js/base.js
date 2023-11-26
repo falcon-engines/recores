26e77d451643010f310b79dc068b2f82245d7302
almuhdilkarim <al.muhdil.0@gmail.com>
2023-11-27 04:54:12 +0700

    falcon dev version 1

--- assets/recores/based/foot/js/base.js
+++ assets/recores/based/foot/js/base.js
@@ -1,1 +1,17 @@
 console.log('recores - falcon engine - foot loaded');
+
+(()=>{
+
+    let navigation_loader_helper = () => {
+        let main_menu = document.getElementById('navigates');
+        main_menu.classList.remove('d-hide');
+    }
+ 
+    window.addEventListener('load', ()=> {
+
+        if ( screen.width > 992  ) {
+            navigation_loader_helper();
+        }
+    })
+   
+})()
